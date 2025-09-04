00250809c6f321ac4772f65d7dd77805ecf5dbe5
Muhammad Ibrahim <mibrahim.code@gmail.com>
2025-09-04 17:36:46 +0500

    Added Clarity code the layout file

--- app/layout.js
+++ app/layout.js
@@ -52,13 +52,14 @@ export default function RootLayout({ children }) {
         <link rel="stylesheet" href="css/responsive.css" />
         <link rel="stylesheet" href="css/rtl.css" />
 
+        {/* Microsoft Clarity Script - Updated with new ID */}
         <Script id="microsoft-clarity" strategy="afterInteractive">
           {`
             (function(c,l,a,r,i,t,y){
               c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
               t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
               y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-            })(window, document, "clarity", "script", "rbkrglex6v");
+            })(window, document, "clarity", "script", "t5hr4n2egd");
           `}
         </Script>
       </head>
