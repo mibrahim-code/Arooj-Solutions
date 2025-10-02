83000596e54475c6a55ca600859caff31e308abe
Muhammad Ibrahim <mibrahim.code@gmail.com>
2025-10-02 15:59:34 +0500

    Added favicon .ico

--- app/layout.js
+++ app/layout.js
@@ -2,21 +2,27 @@ import localFont from "next/font/local";
 import "./globals.css";
 import Script from "next/script";
 
+// Load local fonts
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
   variable: "--font-geist-sans",
   weight: "100 900",
 });
+
 const geistMono = localFont({
   src: "./fonts/GeistMonoVF.woff",
   variable: "--font-geist-mono",
   weight: "100 900",
 });
 
+// ✅ Metadata including favicon
 export const metadata = {
   title: "Arooj Solutions - Cybersecurity & AI Services in Saudi Arabia",
   description:
     "Arooj Solutions provides end-to-end cybersecurity and AI solutions in Saudi Arabia. Protect your digital assets, ensure NCA compliance, and leverage AI for business growth aligned with Vision 2030.",
+  icons: {
+    icon: "/images/aroojsolutionfavicon.ico", // Make sure this file exists in public/images/
+  },
 };
 
 export default function RootLayout({ children }) {
@@ -30,29 +36,29 @@ export default function RootLayout({ children }) {
           content="width=device-width, initial-scale=1, shrink-to-fit=no"
         />
 
-        {/* Favicon */}
-        <link rel="icon" type="image/x-icon" href="/images/as.ico" />
+        {/* ✅ Extra fallback for favicon (optional but good) */}
+        <link rel="icon" type="image/x-icon" href="/images/aroojsolutionfavicon.ico" />
 
-        {/* Links of CSS files */}
-        <link rel="stylesheet" href="css/bootstrap.rtl.min.css" />
-        <link rel="stylesheet" href="css/aos.css" />
-        <link rel="stylesheet" href="css/animate.min.css" />
-        <link rel="stylesheet" href="css/meanmenu.css" />
-        <link rel="stylesheet" href="css/remixicon.css" />
-        <link rel="stylesheet" href="css/odometer.min.css" />
-        <link rel="stylesheet" href="css/owl.carousel.min.css" />
-        <link rel="stylesheet" href="css/owl.theme.default.min.css" />
-        <link rel="stylesheet" href="css/magnific-popup.min.css" />
-        <link rel="stylesheet" href="css/fancybox.css" />
-        <link rel="stylesheet" href="css/nice-select.min.css" />
-        <link rel="stylesheet" href="css/style.css" />
-        <link rel="stylesheet" href="css/navbar.css" />
-        <link rel="stylesheet" href="css/footer.css" />
-        <link rel="stylesheet" href="css/dark.css" />
-        <link rel="stylesheet" href="css/responsive.css" />
-        <link rel="stylesheet" href="css/rtl.css" />
+        {/* CSS Links */}
+        <link rel="stylesheet" href="/css/bootstrap.rtl.min.css" />
+        <link rel="stylesheet" href="/css/aos.css" />
+        <link rel="stylesheet" href="/css/animate.min.css" />
+        <link rel="stylesheet" href="/css/meanmenu.css" />
+        <link rel="stylesheet" href="/css/remixicon.css" />
+        <link rel="stylesheet" href="/css/odometer.min.css" />
+        <link rel="stylesheet" href="/css/owl.carousel.min.css" />
+        <link rel="stylesheet" href="/css/owl.theme.default.min.css" />
+        <link rel="stylesheet" href="/css/magnific-popup.min.css" />
+        <link rel="stylesheet" href="/css/fancybox.css" />
+        <link rel="stylesheet" href="/css/nice-select.min.css" />
+        <link rel="stylesheet" href="/css/style.css" />
+        <link rel="stylesheet" href="/css/navbar.css" />
+        <link rel="stylesheet" href="/css/footer.css" />
+        <link rel="stylesheet" href="/css/dark.css" />
+        <link rel="stylesheet" href="/css/responsive.css" />
+        <link rel="stylesheet" href="/css/rtl.css" />
 
-        {/* Microsoft Clarity Script - Updated with new ID */}
+        {/* Microsoft Clarity Script */}
         <Script id="microsoft-clarity" strategy="afterInteractive">
           {`
             (function(c,l,a,r,i,t,y){
@@ -69,30 +75,32 @@ export default function RootLayout({ children }) {
       >
         {children}
 
-        <Script data-cfasync="false" src="js/email-decode.min.js"></Script>
-        <Script src="js/jquery.min.js"></Script>
-        <Script src="js/bootstrap.bundle.min.js"></Script>
-        <Script src="js/jquery.meanmenu.js"></Script>
-        <Script src="js/owl.carousel.min.js"></Script>
-        <Script src="js/jquery.appear.js"></Script>
-        <Script src="js/odometer.min.js"></Script>
-        <Script src="js/jquery.magnific-popup.min.js"></Script>
-        <Script src="js/fancybox.min.js"></Script>
-        <Script src="js/tweenmax.min.js"></Script>
-        <Script src="js/ScrollMagic.min.js"></Script>
-        <Script src="js/animation.gsap.min.js"></Script>
-        <Script src="js/debug.addIndicators.min.js"></Script>
-        <Script src="js/mixitup.min.js"></Script>
-        <Script src="js/nice-select.min.js"></Script>
-        <Script src="js/tilt.jquery.min.js"></Script>
-        <Script src="js/parallax.min.js"></Script>
-        <Script src="js/jquery.ajaxchimp.min.js"></Script>
-        <Script src="js/form-validator.min.js"></Script>
-        <Script src="js/contact-form-script.js"></Script>
-        <Script src="js/aos.js"></Script>
-        <Script src="js/wow.min.js"></Script>
-        <Script src="js/main.js"></Script>
+        {/* JS Scripts */}
+        <Script data-cfasync="false" src="/js/email-decode.min.js"></Script>
+        <Script src="/js/jquery.min.js"></Script>
+        <Script src="/js/bootstrap.bundle.min.js"></Script>
+        <Script src="/js/jquery.meanmenu.js"></Script>
+        <Script src="/js/owl.carousel.min.js"></Script>
+        <Script src="/js/jquery.appear.js"></Script>
+        <Script src="/js/odometer.min.js"></Script>
+        <Script src="/js/jquery.magnific-popup.min.js"></Script>
+        <Script src="/js/fancybox.min.js"></Script>
+        <Script src="/js/tweenmax.min.js"></Script>
+        <Script src="/js/ScrollMagic.min.js"></Script>
+        <Script src="/js/animation.gsap.min.js"></Script>
+        <Script src="/js/debug.addIndicators.min.js"></Script>
+        <Script src="/js/mixitup.min.js"></Script>
+        <Script src="/js/nice-select.min.js"></Script>
+        <Script src="/js/tilt.jquery.min.js"></Script>
+        <Script src="/js/parallax.min.js"></Script>
+        <Script src="/js/jquery.ajaxchimp.min.js"></Script>
+        <Script src="/js/form-validator.min.js"></Script>
+        <Script src="/js/contact-form-script.js"></Script>
+        <Script src="/js/aos.js"></Script>
+        <Script src="/js/wow.min.js"></Script>
+        <Script src="/js/main.js"></Script>
       </body>
     </html>
   );
 }
+
